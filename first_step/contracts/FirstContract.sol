07c971c625958d0c3f3f253cd2428d2e35f60310
Braejan Arias <barias@sonatype.com>
2022-03-10 19:27:33 -0500

    :construction: First solidity example

---
+++ first_step/contracts/FirstContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract FirstContract {
+    string private name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+
+    function setName(string memory _name) public{
+        name = _name;
+    }
+
+    function getName() public view returns (string memory){
+        return name;
+    }
+    
+}
