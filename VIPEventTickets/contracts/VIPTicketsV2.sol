3334b10688f2d52f00e6a9d1ef2971aadc00c8ba
Braejan Arias <barias@sonatype.com>
2022-05-17 15:30:16 -0500

    :construction: write tests in progress.

--- VIPEventTickets/contracts/VIPTicketsV2.sol
+++ VIPEventTickets/contracts/VIPTicketsV2.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract VIPTicketsV1 is
+contract VIPTicketsV2 is
     ERC20Upgradeable,
     UUPSUpgradeable,
     OwnableUpgradeable
--- VIPEventTickets/test/VIPTickets.js
+++ VIPEventTickets/test/VIPTickets.js
@@ -2,36 +2,63 @@ const{ expect } = require("chai");
 const{ ethers, upgrades } = require("hardhat");
 
 const initialSupply = 10000;
-const tokenName = "VIP Tickets eventName 1";
-const tokenSymbol = "VIPT";
+const tokenName = "Smart Tickets Token";
+const tokenSymbol = "STT";
 const VIPTicketsContractNameV1 = "VIPTicketsV1";
 
-describe("VIP Tickets tests version 1", function() {
-    before(async function () {
-        const availableSigners = await ethers.getSigners();
-        this.deployer = availableSigners[0];
-        const VIPTicketsTokenV1 = await ethers.getContractFactory(VIPTicketsContractNameV1);
-        //this.vipTicketsTokenV1 = await VIPTicketsTokenV1.deploy(tokenName, tokenSymbol, initialSupply);
-        this.vipTicketsTokenV1 = await upgrades.deployProxy(VIPTicketsTokenV1, [tokenName, tokenSymbol, initialSupply]);
-        await this.vipTicketsTokenV1.deployed();
-    });
 
-    it('Should be named VIP Tickets eventName', async function () {
-        const fetchedTokenName = await this.vipTicketsTokenV1.name();
-        expect(fetchedTokenName).to.be.equal(tokenName);
+describe("VIP Tickets general tests", function() {
+    let tokenV1;
+    let tokenV2;
+    let deployer;
+    let userAccount;
+    describe("VIP Tickets tests version 1", function() {
+        before(async function () {
+            const availableSigners = await ethers.getSigners();
+            deployer = availableSigners[0];
+            const VIPTicketsTokenV1 = await ethers.getContractFactory(VIPTicketsContractNameV1);
+
+            //this.vipTicketsTokenV1 = await VIPTicketsTokenV1.deploy(tokenName, tokenSymbol, initialSupply);
+            tokenV1 = await upgrades.deployProxy(VIPTicketsTokenV1, [initialSupply]);
+            await tokenV1.deployed();
+        });
+    
+        it('Should be named VIP Tickets eventName', async function () {
+            const fetchedTokenName = await tokenV1.name();
+            expect(fetchedTokenName).to.be.equal(tokenName);
+        });
+        
+        it('Should has symbol VIPT', async function () {
+            const fetchedTokenName = await tokenV1.symbol();
+            expect(fetchedTokenName).to.be.equal(tokenSymbol);
+        });
+    
+        it('Should have total supply with decimals included.', async function () {
+            const [fetchedTotalSupply, fetchedDecimals] = await Promise.all(
+                [tokenV1.totalSupply(), tokenV1.decimals()]
+            );
+            expectedTotalSupply = ethers.BigNumber.from(initialSupply)
+                .mul(ethers.BigNumber.from(10).pow(fetchedDecimals));
+           expect(fetchedTotalSupply.eq(expectedTotalSupply)).to.be.true; 
+        }); 
     });
     
-    it('Should has symbol VIPT', async function () {
-        const fetchedTokenName = await this.vipTicketsTokenV1.symbol();
-        expect(fetchedTokenName).to.be.equal(tokenSymbol);
-    });
-
-    it('Should have total supply with decimals included.', async function () {
-        const [fetchedTotalSupply, fetchedDecimals] = await Promise.all(
-            [this.vipTicketsTokenV1.totalSupply(), this.vipTicketsTokenV1.decimals()]
-        );
-        expectedTotalSupply = ethers.BigNumber.from(initialSupply)
-            .mul(ethers.BigNumber.from(10).pow(fetchedDecimals));
-       expect(fetchedTotalSupply.eq(expectedTotalSupply)).to.be.true; 
+    
+    describe("VIP Tickets tests version 2", function() {
+        before(async function () {
+            userAccount = (await ethers.getSigners())[1];
+            const VIPTicketsTokenV2 = await ethers.getContractFactory("VIPTicketsV2");
+            tokenV2 = await upgrades.upgradeProxy(tokenV1.address, VIPTicketsTokenV2);
+            await tokenV2.deployed();
+        });
+    
+        it('Should has the same address, and keep the state as the previous version', async function () {
+            const [totalSupplyV1, totalSupplyV2] = await Promise.all([
+                tokenV1.totalSupply(),
+                tokenV2.totalSupply(),
+            ]);
+            expect(tokenV1.address).to.be.equal(tokenV2.address);
+            expect(totalSupplyV1.eq(totalSupplyV2)).to.be.true;
+        }); 
     }); 
 });
